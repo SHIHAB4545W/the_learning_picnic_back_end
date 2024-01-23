df3c73c86dc6f593152588da0012f5567e1be16a
Yara Mohamed <118827093+YarraMohamed@users.noreply.github.com>
2024-01-23 19:04:24 +0200

    Add files via upload

---
+++ models/quizResult.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const resultSchema = new mongoose.Schema ({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    // quizId : {
+    //     type : mongoose.Schema.Types.ObjectId,
+    //     ref :'Quiz',
+    //     required : true
+    // },
+
+    quizGrades : [
+        {
+            quizId :{
+                type : mongoose.Schema.Types.ObjectId,
+                ref :'Quiz',
+                required : true
+            },
+            score :{
+                type : String,
+                required : true
+            }
+        }
+    ]
+})
+
+module.exports = mongoose.model('quizResult', resultSchema);
